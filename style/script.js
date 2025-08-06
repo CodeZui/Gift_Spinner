978c10c4c640c651030cc3441d9f47d87371c44d
Dr. Gifter <phamtuankiet1774@gmail.com>
2025-08-06 14:02:07 +0700

    Add files via upload

---
+++ style/script.js
@@ -0,0 +1,7 @@
+let start = () => {
+  document.getElementById("container").style.display = "none";
+  window.start3 = true;
+  const music = document.getElementById('bg-music');
+  music.play();
+  anim.stop();
+}
